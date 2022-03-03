70087813386c751e6dd72e242717380ea0711332
hitem <hitem@users.noreply.github.com>
2022-03-03 18:13:17 +0100

    Added -s --scope functionalty (manual scope scan)

--- isitup.sh
+++ isitup.sh
@@ -18,7 +18,7 @@ TARGET="$1"
 CURRENT_PATH=$(pwd)
 
 #DEPENDENCIES
-sudo apt-get -y install lolcat hping3
+sudo apt-get -y install lolcat hping3 prips
 clear
 
 #CODE
@@ -31,10 +31,10 @@ echo -e "▀▀▀ ·▀▀▀ ▀▀▀  ▀▀▀ ▀▀  █▪▀▀▀ ▀�
 echo -e ""
 echo -e "$ORANGE              ~:ISITUP:~"
 echo -e "$ORANGE Improve your reconnaissance by$RED hitemSec"
-echo ""
+echo -e ""
 echo -e "$GREEN [+] https://twitter.com/hitemSec"
 echo -e "$GREEN [+] https://github.com/hitem"
-echo -e "$GREEN [-] Usage: isitup.sh [-h --help] [<targetlist>]"
+echo -e "$GREEN [-] Usage: isitup.sh [-h --help] [-s --scope] [<targetlist>]"
 echo -e "$ORANGE __________________________________________"
 exit
 fi
@@ -48,12 +48,58 @@ echo -e "▀▀▀ ·▀▀▀ ▀▀▀  ▀▀▀ ▀▀  █▪▀▀▀ ▀�
 echo -e ""
 echo -e "$ORANGE              ~:ISITUP:~"
 echo -e "$ORANGE Improve your reconnaissance by$RED hitemSec"
-echo ""
+echo -e ""
 echo -e "$GREEN [+] https://twitter.com/hitemSec"
 echo -e "$GREEN [+] https://github.com/hitem"
-echo -e "$GREEN [-]$BLUE Usage: isitup.sh [<targetlist>],$ORANGE example ./isitup.sh myiplist.txt"
-echo -e "$GREEN [-]$BLUE Usage: Modify script to include other ports on line 85"
+echo -e "$GREEN [-]$BLUE Usage: isitup.sh [-h --help] [-s --scope] [targetlist]"
+echo -e "$GREEN [-]$BLUE Usage: Modify script to include other ports on line 85 or 135"
+echo -e "$GREEN [-]$ORANGE Example ./isitup.sh myiplist.txt"
+echo -e "$GREEN [-]$ORANGE Example ./isitup.sh -s$RESET Then proceed to enter your IP-Scope"
+echo -e "$GREEN [-]$ORANGE Example ./isitup.sh --help"
 echo -e "$ORANGE __________________________________________"
+exit
+fi
+
+if [[ $TARGET == "--scope" ]] || [[ $TARGET == "-s" ]]; then
+echo -e " ▄ .▄▪  ▄▄▄▄▄▄▄▄ .• ▌ ▄ ·. .▄▄ · ▄▄▄ . ▄▄·  " | lolcat
+echo -e "██▪▐███ •██  ▀▄.▀··██ ▐███▪▐█ ▀. ▀▄.▀·▐█ ▌▪ " | lolcat
+echo -e "██▀▐█▐█· ▐█.▪▐▀▀▪▄▐█ ▌▐▌▐█·▄▀▀▀█▄▐▀▀▪▄██ ▄▄ " | lolcat
+echo -e "██▌▐▀▐█▌ ▐█▌·▐█▄▄▌██ ██▌▐█▌▐█▄▪▐█▐█▄▄▌▐███▌ " | lolcat
+echo -e "▀▀▀ ·▀▀▀ ▀▀▀  ▀▀▀ ▀▀  █▪▀▀▀ ▀▀▀▀  ▀▀▀ ·▀▀▀  " | lolcat
+echo -e ""
+echo -e "$ORANGE              ~:ISITUP:~"
+echo -e "$ORANGE Improve your reconnaissance by$RED hitemSec"
+echo -e "$ORANGE __________________________________________$RESET"
+echo -e ""
+echo -e "$BLUE Enter a valid IP scope, example$RESET 192.168.0.0/24$BLUE ,$RESET 10.0.1.0/16"
+echo -e "$BLUE If you have selected a large scope, this process will take time! $RESET"
+echo -e ""
+echo -e " Enter your scope and press $IGREEN[ENTER]$RESET to begin"
+read SCOPE
+prips $SCOPE > $CURRENT_PATH/tmp/ManualScope.txt
+echo -e ""
+echo -e " ######################################################     [INITIATING] " | lolcat
+echo -e ""
+for ENTRIES in $(cat $CURRENT_PATH/tmp/ManualScope.txt) 
+do
+    hping3 -S -p 80,443,8080 -c 1 -w 1 $ENTRIES > /dev/null 2>&1
+    if [[ $? -eq 0 ]];
+    then
+        echo -e "$IGREEN [+] $ENTRIES $RESET"
+        echo -e " $ENTRIES" | tee -a $CURRENT_PATH/tmp/valid-$FILENAME > /dev/null 2>&1
+    else
+        echo -e " $ENTRIES" | tee -a $CURRENT_PATH/tmp/notvalid-$FILENAME 
+    fi
+done
+echo -e ""
+echo -e "$BLUE  Valid domains saved to: $ORANGE tmp/valid-$FILENAME  $RESET"
+echo -e "$BLUE  Invalid domains saved to: $ORANGE tmp/notvalid-$FILENAME  $RESET"
+echo -e ""
+ALIVEC=$(cat $CURRENT_PATH/tmp/valid-$FILENAME | sort -u | wc -l)
+DOWNC=$(cat $CURRENT_PATH/tmp/notvalid-$FILENAME | sort -u | wc -l)
+TOTALC=$(cat $CURRENT_PATH/tmp/ManualScope.txt | sort -u | wc -l )
+echo -e "$BLUE  [TOTAL]: ${TOTALC} $RESET     $IGREEN[ALIVE]: ${ALIVEC} $RESET     $IRED[DOWN]: ${DOWNC}"
+echo -e " ######################################################     [COMPLETED] " | lolcat
 exit
 fi
 
@@ -61,7 +107,8 @@ if [ ! -f $TARGET ]; then
 echo -e ""
 echo -e "$IRED ######################     [FILE NOT FOUND] "
 echo -e ""
-echo -e "$GREEN [-]$BLUE Usage: isitup.sh [-h --help] [<targetlist>],$ORANGE example ./isitup.sh myiplist.txt"
+echo -e "$GREEN [+]$BLUE Usage: isitup.sh [targetlist]"
+echo -e "$GREEN [-]$ORANGE Example ./isitup.sh myiplist.txt"
 exit
 fi
 
