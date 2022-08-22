3a2c38c2b2424964e8d3f7aefc954f5ac9c4fb9d
hitem <hitem@users.noreply.github.com>
2022-08-22 19:24:08 +0200

    Added a python (light) version

--- README.md
+++ README.md
@@ -3,21 +3,30 @@
 ISITUP will make your reconnaissance more reliable and validate a chosen list of IP or Domains and output them into a valid and notvalid list.
 ICMP was not enough (from isup), so i rewrote, added and reused some parts and ended up with this TCP based bash script dubbed ISITUP!<br>
 
-## Usage
+# Usage
+## BASH
 ```bash
 > ./isitup.sh list.txt
-> ./isitup.sh [-s --scope]
-> ./isitup.sh [-h --help]
+> ./isitup.sh -s [--scope]
+> ./isitup.sh -h [--help]
 ```
 Outputs two files called ```validlist.txt``` & ```notvalid.txt``` into /tmp/ in scriptdirectory.<br>
-Change ports you want to ping (if any other then ```80```,```443```,```8080``` in the isitup.sh).<br><br>
+Change ports you want to ping (if any other then ```80```,```443```,```8080``` in the ```isitup.sh```).<br><br>
 ![image](https://i.imgur.com/T6qPNTI.gif)
-
 
 ## Dependencies
 Note that ive built in the installation of Hping3, prips and Lolcat. Remove if you have those already.<br>
 ```bash
 > sudo apt-get -y install lolcat hping3 prips
 ```
-## Credits
-Cred to ```gitnepal``` on github and the creator ```@___0x00``` of isup where i started this journey<br>
+
+## Bonus: Python
+```python
+> ./isitup.py
+```
+Before we start: Python version only works with domains (```example.com```) and with http/https (```https://example.com```). \
+It requiers a little more of you to integrate the python version, but once you do its solid! \
+When you luanch the ```isitup.py``` you are asked to enter a ```input``` and ```output``` file, note that the file must contain domainlist with the http/https prefixes, it will go ahead and send a request.get command using http socks and save the successfull requests to the ```outputfile```.
+![image](https://user-images.githubusercontent.com/8977898/185981647-ca686555-c160-4be0-9dd0-e6a70d9ed27c.png) \
+You can hardcore the input/output file to integrate the script into your reconnaissance tools, but in that case i could recommend ```httpx```
+
---
+++ isitup.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+#IMPORTS
+import requests
+import json
+import time
+import sys
+
+#COLORS
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    OKRED = '\033[91m'
+    OKGRAY = '\033[90m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+#VARIABLES TO SET
+#File (and path) to save domains with a successfull response to
+#Example: C:\users\myuser\desktop\subdomains.txt
+#urls_txt = 'urls.txt'
+
+#USER MANUAL INPUTS
+print(f"{bcolors.HEADER}  ▄ .▄▪  ▄▄▄▄▄▄▄▄ .• ▌ ▄ ·. .▄▄ · ▄▄▄ . ▄▄·  \n ██▪▐███ •██  ▀▄.▀··██ ▐███▪▐█ ▀. ▀▄.▀·▐█ ▌▪ \n ██▀▐█▐█· ▐█.▪▐▀▀▪▄▐█ ▌▐▌▐█·▄▀▀▀█▄▐▀▀▪▄██ ▄▄ \n ██▌▐▀▐█▌ ▐█▌·▐█▄▄▌██ ██▌▐█▌▐█▄▪▐█▐█▄▄▌▐███▌ \n ▀▀▀ ·▀▀▀ ▀▀▀  ▀▀▀ ▀▀  █▪▀▀▀ ▀▀▀▀  ▀▀▀ ·▀▀▀  {bcolors.ENDC}")
+print(f"{bcolors.OKGRAY}Improve your reconnaissance by{bcolors.ENDC} {bcolors.OKRED}hitemSec{bcolors.ENDC}")
+print("")
+input_file = input('Enter the file to\033[1m \033[93mimport\033[0m: ')
+output_file = input('Enter the file to\033[1m \033[96mexport\033[0m: ')
+print("")
+
+#THE MAGIC
+try:
+    finput_file = open(input_file,'r').read().splitlines()
+except Exception as e:
+    print(f"{bcolors.OKRED}Please insert a valid file ({bcolors.WARNING}check file or directory{bcolors.OKRED}){bcolors.ENDC}")
+    print(f"{bcolors.OKRED}Error {e}{bcolors.ENDC}")
+    sys.exit(1)
+with open(output_file, 'w') as U:
+    for line in finput_file:
+        #HEADERS
+        headers = {'user-agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:103.0) Gecko/20100101 Firefox/103.0','content-type' : 'application/x-www-form-urlencoded'} 
+        try:
+            #REQUEST
+            line2 = requests.get(line, headers=headers, allow_redirects=True, timeout=5)
+            if line2.ok:
+                #ALL OK REQUESTS (successfull, such as 200, 301)
+                print (f"{bcolors.OKGREEN}[+] [{bcolors.ENDC}",line2.status_code,f"{bcolors.OKGREEN}]{bcolors.ENDC}", line)
+                U.write(line)
+                U.write('\n')
+            else:
+                #ALL FAILED REQUESTS (such as 400-405 etc)
+                print (f"{bcolors.FAIL}[-] [{bcolors.ENDC}",line2.status_code,f"{bcolors.FAIL}]{bcolors.ENDC}", line)
+        except:
+                #THROW A "404" ERROR (If the domain could not be resolved, reached etc)
+                #I troubledshoot this extensivly and this was the easiest and most relayable solution
+            print (f"{bcolors.FAIL}[-] [{bcolors.ENDC}","404",f"{bcolors.FAIL}]{bcolors.ENDC}", line)
+            pass
