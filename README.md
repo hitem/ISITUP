21acf2d6c65adf6d0fb5b65049cb006f73bd2a53
hitem <8977898+hitem@users.noreply.github.com>
2024-06-20 10:08:20 +0200

    Update ISITUP.py: 1. Greatly improved performance and speed 2. Outputfiles depending on responsecode categories

--- README.md
+++ README.md
@@ -32,7 +32,8 @@ Note that ive built in the installation of Hping3, prips and Lolcat. Remove if y
 > sudo apt-get -y install lolcat hping3 prips
 ```
 
-## Bonus: Python
+
+# Python Version
 ```python
 LINUX
 > ./isitup.py -h
@@ -46,7 +47,37 @@ WINDOWS
 > pip install requests urllib3
 > python3 .\isitup.py -h
 ```
-When you launch the ```isitup.py``` you have to supply the following arguments: ```-i <path to file>``` and ```-o <path to file>```, it will go ahead and send a request.get command using http socks and save the successful requests to the ```-o <path to file>```. Note that this tool dont care if you have ```https://```, ```http://``` or ```no prefix``` on the url in the inputfile - if no prefix it will try both just to be sure. \
-<img width="447" alt="Snag_372d1e4" src="https://github.com/hitem/ISITUP/assets/8977898/048989b5-5c0f-41f6-83c6-ce27f3d973ac"> \
-You can integrate the script into your reconnaissance tools and use variables for input/output (xargs or cat -> pipe), but in that case i could recommend ```httpx``` (faster for large lists).
+## Usage
 
+When you launch the `isitup.py` you have to supply the following arguments: `-i` and `-o`. It will send asynchronous HTTP requests and save the results to different output files based on the response status codes. Note that this tool doesn't care if you have `https://`, `http://`, or no prefix on the URL in the input file. If no prefix is provided.
+
+The ISITUP tool then checks if domains are up and organizes the results based on HTTP response codes into different output files. Here is a brief explanation of the different HTTP response code categories and the corresponding output files created by the tool:
+
+1. **Informational Responses (100 – 199)**
+   - These responses indicate that the request was received and understood and is being processed.
+   - Output File: `<output_file_prefix>_1xx.txt`
+   - Example: `102 Processing`
+
+2. **Successful Responses (200 – 299)**
+   - These responses indicate that the request was successfully received, understood, and accepted.
+   - Output File: `<output_file_prefix>_2xx.txt`
+   - Example: `200 OK`
+
+3. **Redirection Messages (300 – 399)**
+   - These responses indicate that further action needs to be taken by the user agent in order to fulfill the request.
+   - Output File: `<output_file_prefix>_3xx.txt`
+   - Example: `301 Moved Permanently`
+
+4. **Client Error Responses (400 – 499)**
+   - These responses indicate that the request contains bad syntax or cannot be fulfilled.
+   - Output File: `<output_file_prefix>_4xx.txt`
+   - Example: `404 Not Found`
+
+5. **Server Error Responses (500 – 599)**
+   - These responses indicate that the server failed to fulfill a valid request.
+   - Output File: `<output_file_prefix>_5xx.txt`
+   - Example: `500 Internal Server Error`
+
+6. **Error Handling**
+   - URLs that encounter errors during the request process are recorded separately.
+   - Output File: `<output_file_prefix>_errors.txt`
--- isitup.py
+++ isitup.py
@@ -12,6 +12,9 @@
 import sys
 import getopt
 import requests
+import socket
+import time
+import concurrent.futures
 from urllib3.exceptions import InsecureRequestWarning
 from urllib3 import disable_warnings
 
@@ -26,8 +29,11 @@ class bcolors:
     OKGRAY = '\033[90m'
     OKRED = '\033[91m'
     WARNING = '\033[93m'
-    FAIL = '\033[91m'
+    INFO = '\033[94m'  # Informational responses color
+    REDIRECT = '\033[93m'  # Orange for redirection messages
+    SERVER_ERROR = '\033[94m'  # Blue for server error responses
     ENDC = '\033[0m'
+    FAIL = '\033[91m'  # Red for failed connections
 
 # Gradient color and logo functions
 def interpolate_color(color1, color2, factor):
@@ -77,11 +83,11 @@ def print_logo_and_instructions():
     {bcolors.OKGRAY}This tool checks if domains are up!{bcolors.ENDC}
 
     {bcolors.WARNING}Usage:{bcolors.ENDC}
-    {bcolors.OKGRAY}python3 .\\isitup.py -i <input_file> -o <output_file>{bcolors.ENDC}
+    {bcolors.OKGRAY}python3 .\\isitup.py -i <input_file> -o <output_file_prefix>{bcolors.ENDC}
     
     {bcolors.WARNING}Examples:{bcolors.ENDC}
-    {bcolors.OKGRAY}isitup.py -i {bcolors.OKCYAN}input_file{bcolors.WARNING} -o {bcolors.OKGREEN}output_file{bcolors.ENDC}
-    {bcolors.OKGRAY}Example: {bcolors.WARNING}.\\isitup.py -i {bcolors.OKCYAN}/root/usr/input.txt{bcolors.WARNING} -o {bcolors.OKGREEN}/root/usr/output.txt{bcolors.ENDC}
+    {bcolors.OKGRAY}isitup.py -i {bcolors.OKCYAN}input_file{bcolors.WARNING} -o {bcolors.OKGREEN}output_file_prefix{bcolors.ENDC}
+    {bcolors.OKGRAY}Example: {bcolors.WARNING}.\\isitup.py -i {bcolors.OKCYAN}/root/usr/input.txt{bcolors.WARNING} -o {bcolors.OKGREEN}/root/usr/output{bcolors.ENDC}
     {bcolors.ENDC}
     """
     print(instructions)
@@ -89,14 +95,76 @@ def print_logo_and_instructions():
 # VARIABLES & File inputs
 print_logo_and_instructions()
 
+# Function to check URL
+def check_url(url, headers, timeout=5, max_retries=2):
+    def dns_resolve(domain):
+        try:
+            socket.gethostbyname(domain)
+            return True
+        except socket.error:
+            return False
+
+    urls_to_try = []
+    if url.startswith("http://") or url.startswith("https://"):
+        domain = url.split("//")[1].split("/")[0]
+        if not dns_resolve(domain):
+            print(f"{bcolors.FAIL}[-] [DNS Error]{bcolors.ENDC} {url}")
+            with open(f"{output_file_prefix}_errors.txt", 'a') as U:
+                U.write(url + '\n')
+            return None
+        urls_to_try.append(url)
+    else:
+        urls_to_try.append(f"https://{url}")
+
+    for url in urls_to_try:
+        for attempt in range(max_retries):
+            try:
+                response = requests.get(url, headers=headers, allow_redirects=True, timeout=timeout, verify=False)
+                status_code = response.status_code
+                if 100 <= status_code < 200:
+                    color = bcolors.INFO
+                    output_file = f"{output_file_prefix}_1xx.txt"
+                elif 200 <= status_code < 300:
+                    color = bcolors.OKGREEN
+                    output_file = f"{output_file_prefix}_2xx.txt"
+                elif 300 <= status_code < 400:
+                    color = bcolors.REDIRECT
+                    output_file = f"{output_file_prefix}_3xx.txt"
+                elif 400 <= status_code < 500:
+                    color = bcolors.OKRED
+                    output_file = f"{output_file_prefix}_4xx.txt"
+                elif 500 <= status_code < 600:
+                    color = bcolors.SERVER_ERROR
+                    output_file = f"{output_file_prefix}_5xx.txt"
+                else:
+                    color = bcolors.ENDC
+                    output_file = f"{output_file_prefix}_others.txt"
+
+                with open(output_file, 'a') as U:
+                    U.write(url + '\n')
+
+                if status_code == 200:
+                    print(f"{color}[+] [{status_code}]{bcolors.ENDC} {url}")
+                    return url
+                else:
+                    print(f"{color}[-] [{status_code}]{bcolors.ENDC} {url}")
+            except requests.exceptions.RequestException:
+                print(f"{bcolors.FAIL}[-] [Error]{bcolors.ENDC} {url}")
+                with open(f"{output_file_prefix}_errors.txt", 'a') as U:
+                    U.write(url + '\n')
+                time.sleep(2 ** attempt)  # Exponential backoff
+                break
+    return None
+
 # Define the main function
 def main(argv):
+    global output_file_prefix
     input_file = ''
-    output_file = ''
+    output_file_prefix = ''
     try:
-        opts, args = getopt.getopt(argv, "hi:o:", ["input_file=", "output_file="])
+        opts, args = getopt.getopt(argv, "hi:o:", ["input_file=", "output_file_prefix="])
     except getopt.GetoptError:
-        print(f"{bcolors.WARNING}isitup.py -i <input_file> -o <output_file>{bcolors.ENDC}")
+        print(f"{bcolors.WARNING}isitup.py -i <input_file> -o <output_file_prefix>{bcolors.ENDC}")
         print("")
         sys.exit(1)
     for opt, arg in opts:
@@ -105,8 +173,8 @@ def main(argv):
             sys.exit(2)
         elif opt in ("-i", "--input_file"):
             input_file = arg
-        elif opt in ("-o", "--output_file"):
-            output_file = arg
+        elif opt in ("-o", "--output_file_prefix"):
+            output_file_prefix = arg
             # THE MAGIC
             try:
                 with open(input_file, 'r') as f:
@@ -117,35 +185,20 @@ def main(argv):
                 print("")
                 sys.exit(3)
 
-            with open(output_file, 'w') as U:
-                for line in lines:
-                    # HEADERS
-                    headers = {
-                        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:113.0) Gecko/20100101 Firefox/127.0',
-                        'content-type': 'application/x-www-form-urlencoded'}
-                    urls_to_try = []
-                    if line.startswith("http://") or line.startswith("https://"):
-                        urls_to_try.append(line)
-                    else:
-                        urls_to_try.append(f"https://{line}")
-                        urls_to_try.append(f"http://{line}")
+            headers = {
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:113.0) Gecko/20100101 Firefox/127.0',
+                'content-type': 'application/x-www-form-urlencoded'}
 
-                    response = None
-                    for url in urls_to_try:
-                        try:
-                            response = requests.get(url, headers=headers, allow_redirects=True, timeout=5, verify=False)
-                            status_code = response.status_code
-                            if status_code == 200:
-                                print(f"{bcolors.OKGREEN}[+] [{bcolors.ENDC}", status_code, f"{bcolors.OKGREEN}]{bcolors.ENDC}", url)
-                                U.write(url)
-                                U.write('\n')
-                                break
-                            else:
-                                print(f"{bcolors.FAIL}[-] [{bcolors.ENDC}", status_code, f"{bcolors.FAIL}]{bcolors.ENDC}", url)
-                        except requests.exceptions.RequestException as e:
-                            print(f"{bcolors.FAIL}[-] [Error]{bcolors.ENDC} {url} - {str(e)}")
-                        except Exception as e:
-                            print(f"{bcolors.FAIL}[-] [Error]{bcolors.ENDC} {url} - {str(e)}")
+            with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+                future_to_url = {executor.submit(check_url, line, headers): line for line in lines}
+                for future in concurrent.futures.as_completed(future_to_url):
+                    url = future_to_url[future]
+                    try:
+                        result = future.result()
+                    except Exception as exc:
+                        print(f"{bcolors.FAIL}[-] [Error]{bcolors.ENDC} {url}")
+                        with open(f"{output_file_prefix}_errors.txt", 'a') as U:
+                            U.write(url + '\n')
 
 # Call the main function
 if __name__ == "__main__":
